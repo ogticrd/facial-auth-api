98847c12318f27bf4115043a92769aafecc8b477
Eddy Decena <eddynelson02@gmail.com>
2021-11-01 10:00:18 -0400

    remove commented code

--- src/face/liveness/__init__.py
+++ src/face/liveness/__init__.py
@@ -1,10 +1,3 @@
-# from .blink import *
-# from .landmarks import *
-# from .blur import *
-# from .lbp import *
-# from .hand import *
-# from .verify_liveness import verify_liveness
-
 from .landmarks import landmarks_detection
 from .blink import blink_detection
 from .blur import blur_detection

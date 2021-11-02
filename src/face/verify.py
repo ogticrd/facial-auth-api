4e686e9fba08c492f0c0682a9b989ec0a1f7f4e2
Eddy Decena <eddynelson02@gmail.com>
2021-11-02 15:24:07 -0400

    Remove TypedDict import

--- src/face/verify.py
+++ src/face/verify.py
@@ -1,12 +1,6 @@
 import os
 import sys
 from typing import Dict
-
-if sys.version_info >= (3, 8):
-    from typing import TypedDict
-else:
-    from typing_extensions import TypedDict
-
 import requests
 from pydantic import BaseModel
 
